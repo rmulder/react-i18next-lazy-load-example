43f2066cbf6c227044d34f55b3f7b518cced7451
Robert Matthew Mulder <rmmulder@gmail.com>
2021-05-06 22:08:03 +0800

    New repo

---
+++ README.md
@@ -0,0 +1,4 @@
+This is the tutorial for achieving localisation for your React app with i18next. Check out the [tutorial](https://bigcheeseapp.com/2020/06/28/translate-your-react-app-with-react-i18next/) if you want to know more.
+
+To get the completed code for the tutorial, please go to [completed code branch](https://github.com/tekminewe/react-i18next-example/tree/feature/completed-tutorial)
+# react-i18next-lazy-load-example
---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import i18next from 'i18next';
+
+import './i18n.config';
+
+const App = () => {
+  const { t } = useTranslation();
+  const [count, setCount] = useState(0);
+  const [selectedLanguage, setSelectedLanguage] = useState('en');
+  const handleLanguageSelect = (event) => {
+    setSelectedLanguage(event.target.value);
+    i18next.changeLanguage(event.target.value);
+    setCount(count + 1);
+  }
+
+  return (
+    <div>
+      <label>
+        English
+        <input
+          type="radio"
+          value="en"
+          checked={selectedLanguage === 'en'}
+          onChange={handleLanguageSelect}
+        />
+      </label>
+      <label>
+        中文
+        <input
+          type="radio"
+          value="zh"
+          checked={selectedLanguage === 'zh'}
+          onChange={handleLanguageSelect}
+        />
+      </label>
+      <p>{t('selectedLanguage', { language: selectedLanguage })}</p>
+      <p>{t('numOfTimesSwitchingLanguage', { count })}</p>
+      <p>{t('welcome')}</p>
+    </div>
+  );
+}
+
+export default App;
